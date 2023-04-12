8a0817e8103b351dd033d8b47220e8cd2f1bb298
Daniel Arias <118637614+AriasDaniel7@users.noreply.github.com>
2023-04-11 19:49:08 -0500

    Finalizando Proyecto

--- src/mundo/pruebas.java
+++ src/mundo/pruebas.java
@@ -14,65 +14,9 @@ import java.util.regex.Pattern;
 public class pruebas {
 
     public static void main(String args[]) {
-        //String cadena = "(((p∨q)∨r)∧¬((p∨q)∧r))";
-        String cadena = "(((p∨q)∨r)∧((p∨q)∧r))";
-        System.out.println(validarCadena(cadena));
-    }
-
-    public static boolean validarCadena(String cadena) {
-        int izq = 0;
-        int der = 0;
-        char u = cadena.charAt(cadena.length() - 1);
-        char p = cadena.charAt(0);
-        boolean a = Pattern.matches("[∨ → ∧ ¬ ↔]", Character.toString(u));
-        boolean b = Pattern.matches("[∨ → ∧ ↔]", Character.toString(p));
-
-        //Validar primero y ultimo
-        if (a || b) {
-            return false;
-        }
-
-        //Validar Parentesis
-        for (int i = 0; i < cadena.length(); i++) {
-            if (cadena.charAt(i) == '(') {
-                izq++;
-            }
-            if (cadena.charAt(i) == ')') {
-                der++;
-            }
-        }
-
-        if (izq != der) {
-            return false;
-        }
-
-        /**
-         * Validar cuando este entre parentesis
-         */
-        for (int i = 0; i < cadena.length() - 2; i++) {
-            char char1 = cadena.charAt(i);
-            char char2 = cadena.charAt(i + 1);
-            char char3 = cadena.charAt(i + 2);
-            boolean entre1 = char3 == ')' && Pattern.matches("[∨ → ¬ ∧ ↔]", Character.toString(char2));
-            boolean entre2 = (char1 == '(' && Pattern.matches("[∨ → ¬ ∧ ↔]", Character.toString(char2)) && char3 == ')');
-            boolean entre3 = (char1 == '(' && Pattern.matches("[∨ → ∧ ↔]", Character.toString(char2)) && Character.isLetter(char3));
-            if (entre1 || entre2 || entre3) {
-                return false;
-            }
-        }
-
-        for (int i = 0; i < cadena.length() - 1; i++) {
-            char char1 = cadena.charAt(i);
-            char char2 = cadena.charAt(i + 1);
-            boolean entre1 = (char1 == char2) && !(char1 == '(' || char1 == ')') && !(char2 == '(' || char2 == ')');
-            boolean entre2 = Character.isLetter(char1) && Character.isLetter(char2);
-            boolean entre3 = Pattern.matches("[∨ → ∧ ↔]", Character.toString(char1)) && Pattern.matches("[∨ → ∧ ↔]", Character.toString(char2));
-            boolean entre4 = Character.isLetter(char1) && char2 == '¬';
-            if (entre1 || entre2 || entre3 || entre4) {
-                return false;
-            }
-        }
-
-        return true;
+        String cadena = "(((p∨q)∨r)∧¬((p∨q)∧r))";
+        tablas t = new tablas(cadena);
+        t.crearTabla();
+        t.imprimirMatriz();
     }
 }
